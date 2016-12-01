fa19888756a0aa530afa94cf9c01cba4078afae4
Emily Wang <lunareclipsite@gmail.com>
2016-11-30 21:00:14 -0500

    Basic version of Book

---
+++ 01Library/Book.java
@@ -0,0 +1,32 @@
+public class Book{
+    private string Author;
+    private string Title;
+    private string ISBN;
+
+    public Book(){
+    }
+    public Book(string a, string t, string i){
+    }
+
+    public string Getauthor(){
+	return Author;
+    }
+    public string Gettitle(){
+	return Title;
+    }
+    public string Getisbn(){
+	return ISBN;
+    }
+    public void Setauthor(string a){
+	Author = a;
+    }
+    public void Settitle(string t){
+	Title = t;
+    }
+    public void Setisbn(string i){
+	ISBN = i;
+    }
+    public string toString(){
+	System.out.println ("+Title", "+Author", "+ISBN+");
+    }
+}
