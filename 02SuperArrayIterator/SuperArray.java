cd8872f097e224c7371a27e4efc2ae8beec9dc7c
Emily Wang <lunareclipsite@gmail.com>
2016-12-08 23:26:28 -0500

    basic layout of how to proceed done, now to write the code and test it

--- 02SuperArrayIterator/SuperArray.java
+++ 02SuperArrayIterator/SuperArray.java
@@ -3,7 +3,9 @@ import java.util.Iterator;
 public class SuperArray implements Iterable<String>{
     private String[] a;
  
-public SuperArray(){}
- public get(int n) {
-     return a[n];
- }
+    public SuperArray(){}
+    
+    public int get(int n) {
+      return a[n];
+    }
+}
--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -3,19 +3,35 @@ public class Barcode implements Comparable<Barcode>{
     private int _checkDigit;
 
     public Barcode(String zip){
-	if (_zip.length() < 5 || _zip.length > 5) {
-	    throw new RuntimeException("zip is not the correct length");
-	}
-	_zip = zip;
+      errortest(zip);
+      _zip = zip;
     }
-    public Barcode clone(){}
     private int CheckSum(){
-	int check = -1;
-	int zel = 0;
-	for (int i = 0; i < _zip.length; i++){
-	    zel +=  _zip.substring(i);
-	}
-	check = zel % 10;
-	return check;
+      int check = -1;
+      int zel = 0;
+      for (int i = 0; i < _zip.length; i++){
+        zel +=  _zip.substring(i);
+      }
+      check = zel % 10;
+      _checkDigit = check;
     }
+    public void errortest(String zip){
+      if (zip.length() < 5 || zip.length() > 6) {
+        throw new IllegalArguementException("zip is not the correct length");
+      }
+     // 4 more exceptions to throw 
+    }
+    public statc int toCode(String zip){
+      errortest(zip);
+      //use if switch statements for zip --> code
+    }
+    //public String toString(){}
+    public int compareTo(String zip, String zip2){
+      int temp = 0;
+      for (int i = 0; i < zip.length() || temp != 0; i++){
+        //find substring, ssubtract, set as variable, return if temp != 0
+      }
+      return temp;
+    }
+    //more methods...?
 }
