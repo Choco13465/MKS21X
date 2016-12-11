8a8b04165b3eef85ab03f186aefe01140208b549
Emily Wang <lunareclipsite@gmail.com>
2016-12-11 00:13:29 -0500

    progress! (and made a mindblowing discovery)

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -4,14 +4,45 @@ public class Barcode// implements Comparable<Barcode>{
 {
     private String _zip;
     private int _checkDigit;
+    public String _code;
 
+//constructor
     public Barcode(String zip){
+      testZip(zip);
+      _zip = zip;
+    }
+    
+//exceptions    
+    public static void testZip(String zip){
       if (zip.length() != 5){
         throw new IllegalArgumentException("zip is not the correct length");
       }
-      _zip = zip;
+      for (int i = 0; i < zip.length(); i++){
+        if (zip.charAt(i) < '0' || zip.charAt(i) > '9'){
+          throw new IllegalArgumentException("zip contains a non digit");
+        }
+      }
     }
+    
+    public static void testCode(String code){
+      if (code.length() != 32){
+        throw new IllegalArgumentException("code is not the correct length");
+      }
+      for (int i = 0; i < code.length(); i++){
+        if (code.charAt(i) != '|' || code.charAt(i) != ':'){
+          throw new IllegalArgumentException("non barcode characters were used");
+        }
+      }
+      if (code.charAt(0) != '|' || code.charAt(31) != '|'){
+        throw new IllegalArgumentException("guard rails are incorrect");
+      }
+      zel = "" + code.substring(1, 30);
+      for (//unfinished
+    }
+    
+//methods
     public static String checkSum(String zip){
+      testZip(zip);
       int zel = 0;
       for (int i = 0; i < zip.length(); i++){
         zel += zip.charAt(i);
@@ -20,43 +51,57 @@ public class Barcode// implements Comparable<Barcode>{
       return zel + "";
     }
     
-    /*   public void errortest(String zip){
-      if (zip.length() < 5 || zip.length() > 5) {
-        throw new IllegalArguementException("zip is not the correct length");
-      }
-      try {
-        int a = 0;
-        for (int i = 0; i < zip; i++){
-          a = (int)(zip.substring(i))
+    public static String toString(String zip){
+      testZip(zip);
+      String zel = "";
+      zel = zel + zip + checkSum(zip) + " " + toCode(zip);
+      return zel;
+    }
+       
+    public static String toCode(String zip){
+      testZip(zip);
+      String zel = "|";
+      for (int i = 0; i < zip.length(); i++){
+        switch(zip.charAt(i)-'0'){
+          case 1: zel += ":::||";
+          break;
+          case 2: zel += "::|:|";
+          break;
+          case 3: zel += "::||:";
+          break;
+          case 4: zel += ":|::|";
+          break;
+          case 5: zel += ":|:|:";
+          break;
+          case 6: zel += ":||::";
+          break;
+          case 7: zel += "|:::|";
+          break;
+          case 8: zel += "|::|:";
+          break;
+          case 9: zel += "|:|::";
+          break;
+          case 0: zel += "||:::";
+          break;
         }
       }
-      catch {IllegalArguementException e){
-        e.printStackTrace();
-      }
-      }
-      
-        
+      zel += "|";
+      return zel;
     }
-    */
-    /*
-    public statc int toCode(String zip){
-      errortest(zip);
-      //use if switch statements for zip --> code
-    }
-*/    
+    
 /*    public static String toZip(String code){
       errortestcode(code);
       for (int i = 1; i < code.length() - 6; i += 5){
    if (code.substring(i,i+5) 
-*/    
-    //public String toString(){}
+*/   
     
-/*    public int compareTo(String zip, String zip2){
+    /*
+    public int compareTo(String zip, String zip2){
       int temp = 0;
       for (int i = 0; i < zip.length() || temp != 0; i++){
         //find substring, ssubtract, set as variable, return if temp != 0
       }
       return temp;
     }
-*/    
+    */
 }
--- 03Barcode/Marcode.java
+++ 03Barcode/Marcode.java
@@ -1,8 +1,10 @@
 public class Marcode {
   public static void main (String [] args) {
-    Barcode s = new Barcode("01010");
+    Barcode s = new Barcode("00010");
     
-    System.out.println (s.checkSum("01010"));
+    System.out.println (s.toCode("77777"));
+    System.out.println (s.toString("77777"));
+    System.out.println (s.checkSum("01890"));
   }
   
 }
